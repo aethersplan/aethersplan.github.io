aaaf6f25be2afb86f547292d11b6bc7acd160cda
erenoguzyesil <jfv.eoy@gmail.com>
2025-10-05 22:49:30 +0300

    Added icons, updated styles and functionality for smaller screens.

--- scripts/navigate-slides.js
+++ scripts/navigate-slides.js
@@ -4,12 +4,16 @@ const secondButton = document.getElementById("second-button");
 const firstArticle = document.getElementById("first");
 const secondArticle = document.getElementById("second");
 
-window.addEventListener("load", () => {
-  window.scrollTo(0, 0);
+let computerViewport = window.innerWidth > 600;
 
-  setTimeout(() => {
-    document.body.style.overflow = "hidden";
-  }, 500);
+window.addEventListener("load", () => {
+  if (computerViewport) {
+    window.scrollTo(0, 0);
+
+    setTimeout(() => {
+      document.body.style.overflow = "hidden";
+    }, 500);
+  }
 });
 
 firstButton.addEventListener("click", () => {
@@ -17,8 +21,14 @@ firstButton.addEventListener("click", () => {
     behavior: "smooth",
   });
 
-  setTimeout(() => {
-    document.body.style.overflowX = "hidden";
-    document.body.style.overflowY = "visible";
-  }, 500);
+  if (computerViewport) {
+    setTimeout(() => {
+      document.body.style.overflowX = "hidden";
+      document.body.style.overflowY = "visible";
+    }, 500);
+  }
+});
+
+window.addEventListener("resize", () => {
+  window.location.reload();
 });
